46dea2aeecbf048f2d7bdacc045e91b54ecc70a4
Chirs Kruger <chris.kruger@gmail.com>
2012-04-26 12:21:21 +0800

    first import of box2d-unity forked files

---
+++ src/Collision/Collision.CollideCircle.cs
@@ -0,0 +1,157 @@
+﻿/*
+  Box2DX Copyright (c) 2009 Ihar Kalasouski http://code.google.com/p/box2dx
+  Box2D original C++ version Copyright (c) 2006-2009 Erin Catto http://www.gphysics.com
+
+  This software is provided 'as-is', without any express or implied
+  warranty.  In no event will the authors be held liable for any damages
+  arising from the use of this software.
+
+  Permission is granted to anyone to use this software for any purpose,
+  including commercial applications, and to alter it and redistribute it
+  freely, subject to the following restrictions:
+
+  1. The origin of this software must not be misrepresented; you must not
+     claim that you wrote the original software. If you use this software
+     in a product, an acknowledgment in the product documentation would be
+     appreciated but is not required.
+  2. Altered source versions must be plainly marked as such, and must not be
+     misrepresented as being the original software.
+  3. This notice may not be removed or altered from any source distribution.
+*/
+
+using Box2DX.Common;
+using UnityEngine;
+
+namespace Box2DX.Collision
+{
+	public partial class Collision
+	{
+		public static void CollideCircles(ref Manifold manifold,
+			CircleShape circle1, Transform xf1, CircleShape circle2, Transform xf2)
+		{
+			manifold.PointCount = 0;
+
+			Vector2 p1 = xf1.TransformPoint(circle1._position); // Common.Math.Mul(xf1, circle1._position);
+			Vector2 p2 = xf2.TransformPoint(circle2._position);
+
+			Vector2 d = p2 - p1;
+			float distSqr = Vector2.Dot(d, d);
+			float radius = circle1._radius + circle2._radius;
+			if (distSqr > radius * radius)
+			{
+				return;
+			}
+
+			manifold.Type = ManifoldType.Circles;
+			manifold.LocalPoint = circle1._position;
+			manifold.LocalPlaneNormal = Vector2.zero;
+			manifold.PointCount = 1;
+
+			manifold.Points[0].LocalPoint = circle2._position;
+			manifold.Points[0].ID.Key = 0;
+		}
+
+		public static void CollidePolygonAndCircle(ref Manifold manifold,
+			PolygonShape polygon, Transform xf1, CircleShape circle, Transform xf2)
+		{
+			manifold.PointCount = 0;
+
+			// Compute circle position in the frame of the polygon.
+			Vector2 c = xf2.TransformPoint(circle._position);
+			Vector2 cLocal = xf1.TransformDirection(c);
+			//Vector2 cLocal = Common.Math.MulT(xf1, c);
+
+			// Find the min separating edge.
+			int normalIndex = 0;
+			float separation = -Settings.FLT_MAX;
+			float radius = polygon._radius + circle._radius;
+			int vertexCount = polygon._vertexCount;
+			Vector2[] vertices = polygon._vertices;
+			Vector2[] normals = polygon._normals;
+
+			for (int i = 0; i < vertexCount; ++i)
+			{
+				float s = Vector2.Dot(normals[i], cLocal - vertices[i]);
+				if (s > radius)
+				{
+					// Early out.
+					return;
+				}
+
+				if (s > separation)
+				{
+					separation = s;
+					normalIndex = i;
+				}
+			}
+
+			// Vertices that subtend the incident face.
+			int vertIndex1 = normalIndex;
+			int vertIndex2 = vertIndex1 + 1 < vertexCount ? vertIndex1 + 1 : 0;
+			Vector2 v1 = vertices[vertIndex1];
+			Vector2 v2 = vertices[vertIndex2];
+
+			// If the center is inside the polygon ...
+			if (separation < Common.Settings.FLT_EPSILON)
+			{
+				manifold.PointCount = 1;
+				manifold.Type = ManifoldType.FaceA;
+				manifold.LocalPlaneNormal = normals[normalIndex];
+				manifold.LocalPoint = 0.5f * (v1 + v2);
+				manifold.Points[0].LocalPoint = circle._position;
+				manifold.Points[0].ID.Key = 0;
+				return;
+			}
+
+			// Compute barycentric coordinates
+			float u1 = Vector2.Dot(cLocal - v1, v2 - v1);
+			float u2 = Vector2.Dot(cLocal - v2, v1 - v2);
+			if (u1 <= 0.0f)
+			{
+				if ((cLocal - v1).SqrMagnitude() > radius * radius)
+				{
+					return;
+				}
+
+				manifold.PointCount = 1;
+				manifold.Type = ManifoldType.FaceA;
+				manifold.LocalPlaneNormal = cLocal - v1;
+				manifold.LocalPlaneNormal.Normalize();
+				manifold.LocalPoint = v1;
+				manifold.Points[0].LocalPoint = circle._position;
+				manifold.Points[0].ID.Key = 0;
+			}
+			else if (u2 <= 0.0f)
+			{
+				if ((cLocal - v2).SqrMagnitude() > radius * radius)
+				{
+					return;
+				}
+
+				manifold.PointCount = 1;
+				manifold.Type = ManifoldType.FaceA;
+				manifold.LocalPlaneNormal = cLocal - v2;
+				manifold.LocalPlaneNormal.Normalize();
+				manifold.LocalPoint = v2;
+				manifold.Points[0].LocalPoint = circle._position;
+				manifold.Points[0].ID.Key = 0;
+			}
+			else
+			{
+				Vector2 faceCenter = 0.5f * (v1 + v2);
+				float separation_ = Vector2.Dot(cLocal - faceCenter, normals[vertIndex1]);
+				if (separation_ > radius)
+				{
+					return;
+				}
+
+				manifold.PointCount = 1;
+				manifold.Type = ManifoldType.FaceA;
+				manifold.LocalPlaneNormal = normals[vertIndex1];
+				manifold.LocalPoint = faceCenter;
+				manifold.Points[0].LocalPoint = circle._position;
+				manifold.Points[0].ID.Key = 0;
+			}
+		}
+	}
+}
---
+++ src/Collision/Collision.CollidePoly.cs
@@ -0,0 +1,311 @@
+﻿/*
+  Box2DX Copyright (c) 2009 Ihar Kalasouski http://code.google.com/p/box2dx
+  Box2D original C++ version Copyright (c) 2006-2009 Erin Catto http://www.gphysics.com
+
+  This software is provided 'as-is', without any express or implied
+  warranty.  In no event will the authors be held liable for any damages
+  arising from the use of this software.
+
+  Permission is granted to anyone to use this software for any purpose,
+  including commercial applications, and to alter it and redistribute it
+  freely, subject to the following restrictions:
+
+  1. The origin of this software must not be misrepresented; you must not
+     claim that you wrote the original software. If you use this software
+     in a product, an acknowledgment in the product documentation would be
+     appreciated but is not required.
+  2. Altered source versions must be plainly marked as such, and must not be
+     misrepresented as being the original software.
+  3. This notice may not be removed or altered from any source distribution.
+*/
+
+using Box2DX.Common;
+using UnityEngine;
+
+namespace Box2DX.Collision
+{
+	public partial class Collision
+	{
+		/// <summary>
+		/// Find the separation between poly1 and poly2 for a give edge normal on poly1.
+		/// </summary>
+		public static float EdgeSeparation(PolygonShape poly1, Transform xf1, int edge1, PolygonShape poly2, Transform xf2)
+		{
+			int count1 = poly1._vertexCount;
+			Vector2[] vertices1 = poly1._vertices;
+			Vector2[] normals1 = poly1._normals;
+
+			int count2 = poly2._vertexCount;
+			Vector2[] vertices2 = poly2._vertices;
+
+			Box2DXDebug.Assert(0 <= edge1 && edge1 < count1);
+
+			// Convert normal from poly1's frame into poly2's frame.
+			Vector2 normal1World = xf1.TransformDirection(normals1[edge1]);
+			Vector2 normal1 = xf2.TransformDirection(normal1World); 
+
+			// Find support vertex on poly2 for -normal.
+			int index = 0;
+			float minDot = Common.Settings.FLT_MAX;
+			for (int i = 0; i < count2; ++i)
+			{
+				float dot = Vector2.Dot(vertices2[i], normal1);
+				if (dot < minDot)
+				{
+					minDot = dot;
+					index = i;
+				}
+			}
+
+			Vector2 v1 = xf1.TransformPoint(vertices1[edge1]);
+			Vector2 v2 = xf2.TransformPoint(vertices2[index]); 
+			float separation = Vector2.Dot(v2 - v1, normal1World);
+			return separation;
+		}
+
+		/// <summary>
+		/// Find the max separation between poly1 and poly2 using edge normals from poly1.
+		/// </summary>
+		public static float FindMaxSeparation(ref int edgeIndex, PolygonShape poly1, Transform xf1, PolygonShape poly2, Transform xf2)
+		{
+			int count1 = poly1._vertexCount;
+			Vector2[] normals1 = poly1._normals;
+
+			// Vector pointing from the centroid of poly1 to the centroid of poly2.
+			Vector2 d = xf2.TransformPoint(poly2._centroid) - xf1.TransformPoint(poly1._centroid);
+			
+			Vector2 dLocal1 = xf1.TransformDirection(d);
+
+			// Find edge normal on poly1 that has the largest projection onto d.
+			int edge = 0;
+			float maxDot = -Common.Settings.FLT_MAX;
+			for (int i = 0; i < count1; ++i)
+			{
+				float dot = Vector2.Dot(normals1[i], dLocal1);
+				if (dot > maxDot)
+				{
+					maxDot = dot;
+					edge = i;
+				}
+			}
+
+			// Get the separation for the edge normal.
+			float s = Collision.EdgeSeparation(poly1, xf1, edge, poly2, xf2);
+
+			// Check the separation for the previous edge normal.
+			int prevEdge = edge - 1 >= 0 ? edge - 1 : count1 - 1;
+			float sPrev = Collision.EdgeSeparation(poly1, xf1, prevEdge, poly2, xf2);
+
+			// Check the separation for the next edge normal.
+			int nextEdge = edge + 1 < count1 ? edge + 1 : 0;
+			float sNext = Collision.EdgeSeparation(poly1, xf1, nextEdge, poly2, xf2);
+
+			// Find the best edge and the search direction.
+			int bestEdge;
+			float bestSeparation;
+			int increment;
+			if (sPrev > s && sPrev > sNext)
+			{
+				increment = -1;
+				bestEdge = prevEdge;
+				bestSeparation = sPrev;
+			}
+			else if (sNext > s)
+			{
+				increment = 1;
+				bestEdge = nextEdge;
+				bestSeparation = sNext;
+			}
+			else
+			{
+				edgeIndex = edge;
+				return s;
+			}
+
+			// Perform a local search for the best edge normal.
+			for (; ; )
+			{
+				if (increment == -1)
+					edge = bestEdge - 1 >= 0 ? bestEdge - 1 : count1 - 1;
+				else
+					edge = bestEdge + 1 < count1 ? bestEdge + 1 : 0;
+
+				s = Collision.EdgeSeparation(poly1, xf1, edge, poly2, xf2);
+
+				if (s > bestSeparation)
+				{
+					bestEdge = edge;
+					bestSeparation = s;
+				}
+				else
+				{
+					break;
+				}
+			}
+
+			edgeIndex = bestEdge;
+			return bestSeparation;
+		}
+
+		public static void FindIncidentEdge(out ClipVertex[] c,
+			PolygonShape poly1, Transform xf1, int edge1, PolygonShape poly2, Transform xf2)
+		{
+			int count1 = poly1._vertexCount;
+			Vector2[] normals1 = poly1._normals;
+
+			int count2 = poly2._vertexCount;
+			Vector2[] vertices2 = poly2._vertices;
+			Vector2[] normals2 = poly2._normals;
+
+			Box2DXDebug.Assert(0 <= edge1 && edge1 < count1);
+
+			// Get the normal of the reference edge in poly2's frame.
+			Vector2 normal1 = xf2.TransformDirection( xf1.TransformDirection(normals1[edge1]));
+
+			// Find the incident edge on poly2.
+			int index = 0;
+			float minDot = Settings.FLT_MAX;
+			for (int i = 0; i < count2; ++i)
+			{
+				float dot = Vector2.Dot(normal1, normals2[i]);
+				if (dot < minDot)
+				{
+					minDot = dot;
+					index = i;
+				}
+			}
+
+			// Build the clip vertices for the incident edge.
+			int i1 = index;
+			int i2 = i1 + 1 < count2 ? i1 + 1 : 0;
+
+			c = new ClipVertex[2];
+
+			c[0].V = xf2.TransformPoint(vertices2[i1]);
+			c[0].ID.Features.ReferenceEdge = (byte)edge1;
+			c[0].ID.Features.IncidentEdge = (byte)i1;
+			c[0].ID.Features.IncidentVertex = 0;
+
+			c[1].V = xf2.TransformPoint(vertices2[i2]);
+			c[1].ID.Features.ReferenceEdge = (byte)edge1;
+			c[1].ID.Features.IncidentEdge = (byte)i2;
+			c[1].ID.Features.IncidentVertex = 1;
+		}
+
+		// Find edge normal of max separation on A - return if separating axis is found
+		// Find edge normal of max separation on B - return if separation axis is found
+		// Choose reference edge as min(minA, minB)
+		// Find incident edge
+		// Clip
+		// The normal points from 1 to 2
+		public static void CollidePolygons(ref Manifold manifold,
+			PolygonShape polyA, Transform xfA, PolygonShape polyB, Transform xfB)
+		{
+			manifold.PointCount = 0;
+			float totalRadius = polyA._radius + polyB._radius;
+
+			int edgeA = 0;
+			float separationA = Collision.FindMaxSeparation(ref edgeA, polyA, xfA, polyB, xfB);
+			if (separationA > totalRadius)
+				return;
+
+			int edgeB = 0;
+			float separationB = Collision.FindMaxSeparation(ref edgeB, polyB, xfB, polyA, xfA);
+			if (separationB > totalRadius)
+				return;
+
+			PolygonShape poly1;	// reference poly
+			PolygonShape poly2;	// incident poly
+			Transform xf1, xf2;
+			int edge1;		// reference edge
+			byte flip;
+			const float k_relativeTol = 0.98f;
+			const float k_absoluteTol = 0.001f;
+
+			if (separationB > k_relativeTol * separationA + k_absoluteTol)
+			{
+				poly1 = polyB;
+				poly2 = polyA;
+				xf1 = xfB;
+				xf2 = xfA;
+				edge1 = edgeB;
+				manifold.Type = ManifoldType.FaceB;
+				flip = 1;
+			}
+			else
+			{
+				poly1 = polyA;
+				poly2 = polyB;
+				xf1 = xfA;
+				xf2 = xfB;
+				edge1 = edgeA;
+				manifold.Type = ManifoldType.FaceA;
+				flip = 0;
+			}
+
+			ClipVertex[] incidentEdge;
+			Collision.FindIncidentEdge(out incidentEdge, poly1, xf1, edge1, poly2, xf2);
+
+			int count1 = poly1._vertexCount;
+			Vector2[] vertices1 = poly1._vertices;
+
+			Vector2 v11 = vertices1[edge1];
+			Vector2 v12 = edge1 + 1 < count1 ? vertices1[edge1 + 1] : vertices1[0];
+
+			Vector2 dv = v12 - v11;
+
+			Vector2 localNormal = dv.CrossScalarPostMultiply(1.0f);
+			localNormal.Normalize();
+			Vector2 planePoint = 0.5f * (v11 + v12);
+
+			Vector2 sideNormal = xf1.TransformDirection(v12 - v11);
+			sideNormal.Normalize();
+			Vector2 frontNormal = sideNormal.CrossScalarPostMultiply(1.0f);
+
+			v11 = xf1.TransformPoint(v11); // Common.Math.Mul(xf1, v11);
+			v12 = xf2.TransformPoint(v12); // Common.Math.Mul(xf1, v12);
+
+			float frontOffset = Vector2.Dot(frontNormal, v11);
+			float sideOffset1 = -Vector2.Dot(sideNormal, v11);
+			float sideOffset2 = Vector2.Dot(sideNormal, v12);
+
+			// Clip incident edge against extruded edge1 side edges.
+			ClipVertex[] clipPoints1;
+			ClipVertex[] clipPoints2;
+			int np;
+
+			// Clip to box side 1
+			np = Collision.ClipSegmentToLine(out clipPoints1, incidentEdge, -sideNormal, sideOffset1);
+
+			if (np < 2)
+				return;
+
+			// Clip to negative box side 1
+			np = ClipSegmentToLine(out clipPoints2, clipPoints1, sideNormal, sideOffset2);
+
+			if (np < 2)
+				return;
+
+			// Now clipPoints2 contains the clipped points.
+			manifold.LocalPlaneNormal = localNormal;
+			manifold.LocalPoint = planePoint;
+
+			int pointCount = 0;
+			for (int i = 0; i < Settings.MaxManifoldPoints; ++i)
+			{
+				float separation = Vector2.Dot(frontNormal, clipPoints2[i].V) - frontOffset;
+
+				if (separation <= totalRadius)
+				{
+					ManifoldPoint cp = manifold.Points[pointCount];
+					cp.LocalPoint = Common.Math.MulT(xf2, clipPoints2[i].V);
+					cp.ID = clipPoints2[i].ID;
+					cp.ID.Features.Flip = flip;
+					++pointCount;
+				}
+			}
+
+			manifold.PointCount = pointCount;
+		}
+	}
+}
